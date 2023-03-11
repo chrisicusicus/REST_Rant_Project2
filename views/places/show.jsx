3b26d3ad137d06eb540a70e736336ab5fbb2e3d8
chrisicusicus <115127962+chrisicusicus@users.noreply.github.com>
2023-03-10 21:06:32 -0500

    fiddled w show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,26 +1,55 @@
 const React = require('react')
 const Def = require('../default')
 
+const React = require('react')
+const Def = require('../default')
+
 function show (data) {
     return (
         <Def>
           <main>
+            <div className="row">
+              <div className="col-sm-6">
+                <img src={data.place.pic} alt={data.place.name}/>
+                <h3> Located in {data.place.city}, {data.place.state} </h3>
+              </div>
+              <div className="col-sm-6">
+                <h2> Description </h2>
+                <h3> {data.place.showEstablished()} </h3>
+                <h4> Serving {data.place.cuisines} </h4>
+              </div>
+            </div>
             <h1>{ data.place.name }</h1>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-  Edit
-</a>  
-<form method="POST" action=""> 
-  <button type="submit" className="btn btn-danger">
-    Delete
-  </button>
-</form>     
-
+            <img> {data.place.img } </img>
+            <h2></h2>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> Edit </a>
+            <form method = "POST" action="">
+              <button type="submit" classname= "btn btn-danger"> Delete </button>
+            </form>
           </main>
         </Def>
     )
 }
 
-  
-
-
 module.exports = show
+
+
+
+// function show (data) {
+//     return (
+//         <Def>
+//           <main>
+//             <h1>{ data.place.name }</h1>
+//             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+//   Edit
+// </a>  
+// <form method="POST" action=""> 
+//   <button type="submit" className="btn btn-danger">
+//     Delete
+//   </button>
+// </form>     
+
+//           </main>
+//         </Def>
+//     )
+// }
